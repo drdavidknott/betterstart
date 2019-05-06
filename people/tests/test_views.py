b5b33b9b3614b438422a0805f4ad56af55d5ba7c
David Knott <dkoysta@gmail.com>
2019-05-06 15:36:46 +0100

    Added tests for people search.

--- people/tests/test_views.py
+++ people/tests/test_views.py
@@ -5,26 +5,27 @@ import datetime
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-def set_up_100_test_people():
-# create two role types that our test people will belong to
-	test_role_type_1 = Role_Type.objects.create(role_type_name='test_role_1')
-	test_role_type_2 = Role_Type.objects.create(role_type_name='test_role_1')
-	# and the ethnicity
+def set_up_people_base_data():
+	# set up base data needed to do tests for people
+	# first the ethnicity
 	test_ethnicity = Ethnicity.objects.create(description='test_ethnicity')
 	# and the capture type
 	test_capture_type = Capture_Type.objects.create(capture_type_name='test_capture_type')
-	# create 50 people for role type 1
-	for n in range(1,50):
+
+def set_up_test_people(name_root,role_type,number):
+	# set up the number of people asked for
+	# create the number of people needed
+	for n in range(number):
 		# create a person
 		test_person = Person.objects.create(
-											first_name = 'First_' + str(n),
+											first_name = name_root + str(n),
 											middle_names = 'Middle Names',
-											last_name = 'Last',
+											last_name = name_root + str(n),
 											email_address = 'test@test.com',
 											date_of_birth = datetime.datetime.strptime('2000-01-01','%Y-%m-%d'),
 											gender = 'Gender',
 											notes = 'test notes',
-											default_role = test_role_type_1,
+											default_role = role_type,
 											english_is_second_language = False,
 											pregnant = False,
 											due_date = None
@@ -32,28 +33,7 @@ def set_up_100_test_people():
 		# create a role history entry
 		Role_History.objects.create(
 									person = test_person,
-									role_type = test_role_type_1
-									)
-	# create 50 people for role type 2
-	for n in range(51,100):
-		# create a person
-		test_person = Person.objects.create(
-											first_name = 'First_' + str(n),
-											middle_names = 'Middle Names',
-											last_name = 'Last',
-											email_address = 'test@test.com',
-											date_of_birth = datetime.datetime.strptime('2000-01-01','%Y-%m-%d'),
-											gender = 'Gender',
-											notes = 'test notes',
-											default_role = test_role_type_2,
-											english_is_second_language = False,
-											pregnant = False,
-											due_date = None
-											)
-		# create a role history entry
-		Role_History.objects.create(
-									person = test_person,
-									role_type = test_role_type_2
+									role_type = role_type
 									)
 
 def set_up_test_user():
@@ -65,8 +45,29 @@ def set_up_test_user():
 class PeopleViewTest(TestCase):
 	@classmethod
 	def setUpTestData(cls):
-		# Create 100 people for the test
-		set_up_100_test_people()
+		# create a test user
+		user = set_up_test_user()
+		# create the Parent role
+		parent_role = Role_Type.objects.create(role_type_name='parent')
+		# and the parent champion role
+		parent_champion_role = Role_Type.objects.create(role_type_name='parent champion')
+		# and three more test role types
+		test_role_1 = Role_Type.objects.create(role_type_name='test role 1')
+		test_role_2 = Role_Type.objects.create(role_type_name='test role 2')
+		test_role_3 = Role_Type.objects.create(role_type_name='test role 3')
+		# Create 50 of each type
+		set_up_test_people('Parent_',parent_role,50)
+		set_up_test_people('Parent_Champion_',parent_champion_role,50)
+		set_up_test_people('Test_Role_1_',test_role_1,50)
+		set_up_test_people('Test_Role_2_',test_role_2,50)
+		# and 50 of each of the two test role types with different names
+		set_up_test_people('Different_Name_',test_role_1,50)
+		set_up_test_people('Another_Name_',test_role_2,50)
+		# and more with the roles swapped over
+		set_up_test_people('Different_Name_',test_role_2,50)
+		set_up_test_people('Another_Name_',test_role_1,50)
+		# and a short set to test a result set with less than a page
+		set_up_test_people('Short_Set_',test_role_3,10)
 
 	def test_redirect_if_not_logged_in(self):
 		# get the response
@@ -75,8 +76,6 @@ class PeopleViewTest(TestCase):
 		self.assertRedirects(response, '/people/login?next=/listpeople')
 
 	def test_empty_page_if_logged_in(self):
-		# create a test user
-		user = set_up_test_user()
 		# log the user in
 		self.client.login(username='testuser', password='testword')
 		# attempt to get the events page
@@ -85,3 +84,133 @@ class PeopleViewTest(TestCase):
 		self.assertEqual(response.status_code, 200)
 		# the list of people passed in the context should be empty
 		self.assertEqual(len(response.context['people']),0)
+
+	def test_search_with_no_criteria(self):
+		# log the user in
+		self.client.login(username='testuser', password='testword')
+		# attempt to get the events page
+		response = self.client.post(
+									reverse('listpeople'),
+									data = { 
+											'action' : 'search',
+											'first_name' : '',
+											'last_name' : '',
+											'role_type' : '0',
+											'page' : '1'
+											}
+									)
+		# check that we got a response
+		self.assertEqual(response.status_code, 200)
+		# check how many we got for this page
+		self.assertEqual(len(response.context['people']),25)
+		# check that we got the right number of pages
+		self.assertEqual(response.context['page_list'],[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
+
+	def test_search_for_parent_role_type(self):
+		# log the user in
+		self.client.login(username='testuser', password='testword')
+		# get the parent role type record
+		parent_role_type = Role_Type.objects.get(role_type_name='parent')
+		# attempt to get the events page
+		response = self.client.post(
+									reverse('listpeople'),
+									data = { 
+											'action' : 'search',
+											'first_name' : '',
+											'last_name' : '',
+											'role_type' : str(parent_role_type.pk),
+											'page' : '1'
+											}
+									)
+		# check that we got a response
+		self.assertEqual(response.status_code, 200)
+		# check how many we got for this page
+		self.assertEqual(len(response.context['people']),25)
+		# check that we got the right number of pages
+		self.assertEqual(response.context['page_list'],[1,2])
+
+	def test_search_by_name_with_matching_case(self):
+		# log the user in
+		self.client.login(username='testuser', password='testword')
+		# attempt to get the events page
+		response = self.client.post(
+									reverse('listpeople'),
+									data = { 
+											'action' : 'search',
+											'first_name' : 'Test_Role_1',
+											'last_name' : '',
+											'role_type' : '0',
+											'page' : '1'
+											}
+									)
+		# check that we got a response
+		self.assertEqual(response.status_code, 200)
+		# check how many we got for this page
+		self.assertEqual(len(response.context['people']),25)
+		# check that we got the right number of pages
+		self.assertEqual(response.context['page_list'],[1,2])
+
+	def test_search_by_name_with_non_matching_case(self):
+		# log the user in
+		self.client.login(username='testuser', password='testword')
+		# attempt to get the events page
+		response = self.client.post(
+									reverse('listpeople'),
+									data = { 
+											'action' : 'search',
+											'first_name' : 'test_role_1',
+											'last_name' : '',
+											'role_type' : '0',
+											'page' : '1'
+											}
+									)
+		# check that we got a response
+		self.assertEqual(response.status_code, 200)
+		# check how many we got for this page
+		self.assertEqual(len(response.context['people']),25)
+		# check that we got the right number of pages
+		self.assertEqual(response.context['page_list'],[1,2])
+
+	def test_search_for_short_set(self):
+		# log the user in
+		self.client.login(username='testuser', password='testword')
+		# attempt to get the events page
+		response = self.client.post(
+									reverse('listpeople'),
+									data = { 
+											'action' : 'search',
+											'first_name' : 'Short',
+											'last_name' : '',
+											'role_type' : '0',
+											'page' : '1'
+											}
+									)
+		# check that we got a response
+		self.assertEqual(response.status_code, 200)
+		# check how many we got for this page
+		self.assertEqual(len(response.context['people']),10)
+		# check that we got the right number of pages
+		self.assertEqual(response.context['page_list'],False)
+
+	def test_search_by_name_and_role_type(self):
+		# log the user in
+		self.client.login(username='testuser', password='testword')
+		# get the test role type record
+		test_role_type_1 = Role_Type.objects.get(role_type_name='test role 1')
+		# attempt to get the events page
+		response = self.client.post(
+									reverse('listpeople'),
+									data = { 
+											'action' : 'search',
+											'first_name' : 'Different',
+											'last_name' : '',
+											'role_type' : str(test_role_type_1.pk),
+											'page' : '1'
+											}
+									)
+		# check that we got a response
+		self.assertEqual(response.status_code, 200)
+		# check how many we got for this page
+		self.assertEqual(len(response.context['people']),25)
+		# check that we got the right number of pages
+		self.assertEqual(response.context['page_list'],[1,2])
